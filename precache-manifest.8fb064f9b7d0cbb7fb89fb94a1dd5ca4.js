7a3de39fd4a4e906a5774a7c8a65358698769505
huyem040701 <75148440+huyem040701@users.noreply.github.com>
2021-10-19 22:49:43 +0700

    Updates

---
+++ precache-manifest.8fb064f9b7d0cbb7fb89fb94a1dd5ca4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "608dc3ee15d52f3784ce2c8df7c6f94a",
+    "url": "/TodoAPP/index.html"
+  },
+  {
+    "revision": "11a15c2391bb3f9ed98d",
+    "url": "/TodoAPP/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "23665a6191e060677181",
+    "url": "/TodoAPP/static/css/main.5820d271.chunk.css"
+  },
+  {
+    "revision": "11a15c2391bb3f9ed98d",
+    "url": "/TodoAPP/static/js/2.ca743ca3.chunk.js"
+  },
+  {
+    "revision": "23665a6191e060677181",
+    "url": "/TodoAPP/static/js/main.fbd9efbf.chunk.js"
+  },
+  {
+    "revision": "0011d4925b5c61f120bd",
+    "url": "/TodoAPP/static/js/runtime~main.701e6170.js"
+  }
+]);
